79c59be99f4315bd4f229f57435718acac546c31
Paul MacRory <pmacro@icloud.com>
2019-05-15 23:12:33 +0100

    Add explicit diagnostic severity type.

--- Sources/LanguageClient/Diagnostics.swift
+++ Sources/LanguageClient/Diagnostics.swift
@@ -16,13 +16,29 @@ public struct DiagnosticsParams: Decodable {
   let diagnostics: [Diagnostic]
 }
 
+public enum DiagnosticSeverity: Int, Decodable {
+  
+  // Reports an error.
+  case error = 1
+  
+  // Reports a warning.
+  case warning = 2
+  
+  // Reports an information.
+  case information = 3
+  
+  // Reports a hint.
+  case hint = 4
+}
+
+
 public struct Diagnostic: Decodable {
   /// The range at which the message applies.
   public let range: ContentRange
   
   /// The diagnostic's severity. Can be omitted. If omitted it is up to the
   /// client to interpret diagnostics as error, warning, info or hint.
-  public let severity: Int?
+  public let severity: DiagnosticSeverity?
   
   /// The diagnostic's code. Can be omitted.
   public let code: String?
@@ -33,4 +49,14 @@ public struct Diagnostic: Decodable {
   
   /// The diagnostic's message.
   public let message: String
+}
+
+extension Array where Element == Diagnostic {
+  
+  /// The most severe diagnostic in the array.
+  public var mostSevere: Diagnostic? {
+    return self.sorted(by: { (lhs, rhs) -> Bool in
+      (lhs.severity?.rawValue ?? 1) < (rhs.severity?.rawValue ?? 1)
+    }).first
+  }
 }
